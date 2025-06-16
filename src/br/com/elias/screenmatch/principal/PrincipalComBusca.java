2593233a44779b7a37c3f5403950e9561840102a
elias <eliascamilo70@outlook.com>
2025-06-16 11:25:11 -0300

    Implementação de integração com API OMDb API

---
+++ src/br/com/elias/screenmatch/principal/PrincipalComBusca.java
@@ -0,0 +1,16 @@
+package br.com.elias.screenmatch.principal;
+import java.io.IOException;
+import java.net.URI;
+import java.net.http.HttpClient;
+import java.net.http.HttpRequest;
+import java.net.http.HttpResponse;
+
+public class PrincipalComBusca {
+    public static void main(String [] args) throws IOException, InterruptedException {
+        HttpClient client = HttpClient.newHttpClient();
+        HttpRequest request = HttpRequest.newBuilder().uri(URI.create("https://www.omdbapi.com/?i=tt3896198&apikey=65a969fa")).build();
+
+        HttpResponse<String> response = client.send(request, HttpResponse.BodyHandlers.ofString());
+        System.out.println(response.body());
+    }
+}
--- src/br/com/elias/screenmatch/principal/PrincipalComListas.java
+++ src/br/com/elias/screenmatch/principal/PrincipalComListas.java
@@ -4,9 +4,7 @@ import br.com.elias.screenmatch.modelos.Filme;
 import br.com.elias.screenmatch.modelos.Serie;
 import br.com.elias.screenmatch.modelos.Titulo;
 
-import java.util.ArrayList;
-import java.util.Collections;
-import java.util.Comparator;
+import java.util.*;
 
 public class PrincipalComListas {
     public static void main(String[] args) {
@@ -36,7 +34,7 @@ public class PrincipalComListas {
     }
 
 
-    ArrayList<String> buscaPorArtista = new ArrayList<>();
+    List<String> buscaPorArtista = new LinkedList<>();
     buscaPorArtista.add("Ana De Armas");
     buscaPorArtista.add("Elias");
     buscaPorArtista.add("MijorButMajor");
